778e05170478d479f4fba97b29ae372e4e9fea47
Emilio Bensignor <liobensignor@gmail.com>
2025-04-14 16:44:13 -0300

    Fix audit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,20 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ["~/assets/main.css"],
-  modules: ["@primevue/nuxt-module", "@nuxt/image", "@nuxt/icon", "@nuxtjs/seo", "nuxt-vitalizer", 'nuxt-booster', '@nuxtjs/fontaine', 'nuxt-anchorscroll', '@nuxt/scripts', 'nuxt-swiper', '@nuxtjs/supabase', '@pinia/nuxt'],
+  modules: [
+    "@primevue/nuxt-module",
+    "@nuxt/image",
+    "@nuxt/icon",
+    "@nuxtjs/seo",
+    "nuxt-vitalizer",
+    'nuxt-booster',
+    '@nuxtjs/fontaine',
+    'nuxt-anchorscroll',
+    '@nuxt/scripts',
+    'nuxt-swiper',
+    '@nuxtjs/supabase',
+    '@pinia/nuxt'
+  ],
   supabase: {
     redirect: false,
   },
